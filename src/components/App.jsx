29fca5ccb5edaf3475e42b391d16fb1e0eb3fe27
dmgrabski <dmgrabski97@gmail.com>
2024-01-30 19:47:43 +0100

    update3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,110 +1,128 @@
-import { useEffect, useState } from "react";
+import React from 'react';
 import "./App.css";
-import Searchbar from "./components/Searchbar/Searchbar";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import Loader from "./components/Loader/Loader";
-import ImageGalleryItem from "./components/ImageGalleryItem/ImageGalleryItem";
-import Button from "./components/Button/Button";
+import Searchbar from "./Searchbar";
+import ImageGallery from "./ImageGallery";
+import Loader from "./Loader";
+import ImageGalleryItem from "./ImageGalleryItem";
+import Button from "./Button";
 import axios from "axios";
-import Modal from "./components/Modal/Modal";
-import styles from "./components/Button/Button.module.css";
+import Modal from "./Modal";
 
-function App() {
-  const API_KEY = "40965996-859f4faa7c889b6c9b25dbc7d";
-  const [results, setResults] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [showLoadMore, setShowLoadMore] = useState(false);
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(0);
-  const [lastSearchQuery, setLastSearchQuery] = useState("");
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === "Escape" && isModalOpen) toggleModal();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isLoading: false,
+      isModalOpen: false,
+      selectedImage: null,
+      showLoadMore: false,
+      page: 1,
+      total: 0,
+      lastSearchQuery: ""
     };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [isModalOpen]);
+    this.API_KEY = "40965996-859f4faa7c889b6c9b25dbc7d";
+  }
 
-  useEffect(() => {
-    setShowLoadMore(Math.ceil(total / 12) > 1);
-  }, [total, results]);
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
 
-  const fetchData = async function (query, page) {
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.total !== prevState.total || this.state.results !== prevState.results) {
+      this.setState({ showLoadMore: Math.ceil(this.state.total / 12) > 1 });
+    }
+  }
+
+  handleKeyDown = (e) => {
+    if (e.key === "Escape" && this.state.isModalOpen) {
+      this.toggleModal();
+    }
+  };
+
+  fetchData = async (query) => {
     try {
-      setIsLoading(true);
+      this.setState({ isLoading: true });
       const res = await axios.get("https://pixabay.com/api/", {
         params: {
           q: query,
-          page: page,
-          key: API_KEY,
+          page: this.state.page,
+          key: this.API_KEY,
           image_type: "photo",
           orientation: "horizontal",
           per_page: 12,
         },
       });
-      setIsLoading(false);
-      setResults((prevResults) => [...res.data.hits, ...prevResults]);
-      setTotal(Math.ceil(res.data.totalHits / res.config.params.per_page));
+      this.setState({
+        isLoading: false,
+        results: [...res.data.hits, ...this.state.results],
+        total: Math.ceil(res.data.totalHits / res.config.params.per_page)
+      });
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  const handleSubmit = async (query) => {
-    setLastSearchQuery(query);
-    setResults([]);
-    await fetchData(query);
-    setPage(1);
-  };
-  const toggleModal = (image) => {
-    setSelectedImage(image);
-    setIsModalOpen(!isModalOpen);
+  handleSubmit = async (query) => {
+    this.setState({ lastSearchQuery: query, results: [] });
+    await this.fetchData(query);
   };
 
-  const handleLoadMore = async () => {
-    setPage((prevPage) => prevPage + 1);
-    fetchData(lastSearchQuery, page + 1);
+  toggleModal = (image) => {
+    this.setState({
+      selectedImage: image,
+      isModalOpen: !this.state.isModalOpen
+    });
   };
-  return (
-    <>
-      <Searchbar onSubmit={handleSubmit} />
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <ImageGallery>
-          {results &&
-            results.map((result) => (
-              <ImageGalleryItem
-                onClick={() => toggleModal(result.largeImageURL)}
-                key={result.id}
-                src={result.webformatURL}
-                description={result.description}
-              />
-            ))}
-        </ImageGallery>
-      )}
-      {showLoadMore && (
-        <Button
-          nextPage={handleLoadMore}
-          className={styles.loadmore}
-          type="button"
-          label="Load more"
-        />
-      )}
-      {isModalOpen && (
-        <Modal
-          imageURL={selectedImage}
-          closeModal={toggleModal}
-          onClick={toggleModal}
-        />
-      )}
-    </>
-  );
+
+  handleLoadMore = async () => {
+    await this.fetchData(this.state.lastSearchQuery);
+    this.setState((prevState) => ({ page: prevState.page + 1 }));
+  };
+
+  render() {
+    const { isLoading, results, showLoadMore, isModalOpen, selectedImage } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSubmit} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <ImageGallery>
+            {results &&
+              results.map((result) => (
+                <ImageGalleryItem
+                  onClick={() => this.toggleModal(result.largeImageURL)}
+                  key={result.id}
+                  src={result.webformatURL}
+                  description={result.description}
+                />
+              ))}
+          </ImageGallery>
+        )}
+        {showLoadMore && (
+          <Button
+            nextPage={this.handleLoadMore}
+            className={"load-more"}
+            type="button"
+            label="Load more"
+          />
+        )}
+        {isModalOpen && (
+          <Modal
+            imageURL={selectedImage}
+            closeModal={this.toggleModal}
+            onClick={this.toggleModal}
+          />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;

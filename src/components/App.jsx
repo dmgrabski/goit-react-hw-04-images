cb02d8cfc6cba4729c89903fc815962c5140269e
dmgrabski <dmgrabski97@gmail.com>
2024-01-31 18:52:08 +0100

    updatefolder1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useCallback } from "react";
-import "../App.css";
+import "./App.css";
 import Searchbar from "../components/Searchbar/Searchbar";
 import ImageGallery from "../components/ImageGallery/ImageGallery";
 import Loader from "../components/Loader/Loader";
